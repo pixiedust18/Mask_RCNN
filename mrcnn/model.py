57959c163a5a04bc3f41a536f76dc9405f73b1d1
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2020-05-18 21:27:04 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2185,8 +2185,7 @@ class MaskRCNN():
         self.keras_model.add_loss(tf.add_n(reg_losses))
 
         # Compile
-        self.keras_model.compile(metrics = ['accuracy']
-            optimizer=optimizer,
+        self.keras_model.compile(optimizer=optimizer,
             loss=[None] * len(self.keras_model.outputs))
 
         # Add metrics for losses
